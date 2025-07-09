da12878249370a8c2275fb1b843c570930c82591
Maksalina95 <maksalinamagazin@gmail.com>
2025-07-09 23:20:37 +0300

    Update menu.js
    О

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,17 +1,14 @@
-document.getElementById('burgerBtn').addEventListener('click', () => {
-  const menu = document.getElementById('menu');
-  menu.classList.toggle('hidden');
-});
-
 document.addEventListener('DOMContentLoaded', () => {
+  const burgerBtn = document.getElementById('burgerBtn');
   const menu = document.getElementById('menu');
-  menu.innerHTML = `
-    <ul>
-      <li><a href="catalog.html">Каталог</a></li>
-      <li><a href="favorites.html">Избранное</a></li>
-      <li><a href="delivery.html">Доставка</a></li>
-      <li><a href="contacts.html">Контакты</a></li>
-      <li><a href="rules.html">Условия</a></li>
-    </ul>
-  `;
+
+  burgerBtn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+  });
+
+  document.addEventListener('click', (e) => {
+    if (!menu.contains(e.target) && !burgerBtn.contains(e.target)) {
+      menu.classList.add('hidden');
+    }
+  });
 });
