f5a766800e9d0df865fe2a623007fd63751700c8
Maksalina95 <maksalinamagazin@gmail.com>
2025-07-10 11:36:13 +0300

    Update menu.js
    О

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,12 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const menu = document.getElementById('menu');
 
   burgerBtn.addEventListener('click', () => {
-    menu.classList.toggle('hidden');
+    menu.classList.toggle('active'); // Показываем или скрываем меню
   });
 
   document.addEventListener('click', (e) => {
     if (!menu.contains(e.target) && !burgerBtn.contains(e.target)) {
-      menu.classList.add('hidden');
+      menu.classList.remove('active'); // Скрываем меню при клике вне его
     }
   });
 });
