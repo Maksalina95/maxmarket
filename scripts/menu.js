e7c2b0510bae57004b3e97c9f8c35409207961ea
Maksalina95 <maksalinamagazin@gmail.com>
2025-07-11 13:29:55 +0300

    Update menu.js
    О

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,6 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const burgerBtn = document.getElementById('burgerBtn');
   const menu = document.getElementById('menu');
 
+  <li>
+  <button onclick="toggleCategoryMenu()">Каталог ▼</button>
+  <ul id="dynamicCategoryMenu" class="hidden"></ul>
+</li>
+                          
   burgerBtn.addEventListener('click', () => {
     menu.classList.toggle('active'); // Показываем или скрываем меню
   });
